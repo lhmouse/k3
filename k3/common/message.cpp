d848f5bfe2e845b5b37603bd9ff039fe3d93fc4c
LIU Hao <lh_mouse@126.com>
2024-06-21 19:02:37 +0800

    Z

---
+++ k3/common/message.cpp
@@ -0,0 +1,10 @@
+// This file is part of k3.
+// Copyleft 2024, LH_Mouse. All wrongs reserved.
+
+#include "../xprecompiled.hpp"
+#include "message.hpp"
+namespace k3 {
+
+
+
+}  // namespace k3
---
+++ k3/common/message.hpp
@@ -0,0 +1,21 @@
+// This file is part of k3.
+// Copyleft 2024, LH_Mouse. All wrongs reserved.
+
+#ifndef K3VR5NZE_COMMON_MESSAGE_
+#define K3VR5NZE_COMMON_MESSAGE_
+
+#include "../fwd.hpp"
+namespace k3 {
+
+struct Message
+  {
+
+  };
+
+inline
+void
+swap(Message& lhs, Message& rhs) noexcept
+  { lhs.swap(rhs);  }
+
+}  // namespace k3
+#endif
