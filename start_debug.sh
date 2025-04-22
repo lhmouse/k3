c5e901dd22f7deb1c230076386797cb36eb525d5
LIU Hao <lh_mouse@126.com>
2025-04-22 19:00:29 +0800

    build: Disable sanitizers

--- start_debug.sh
+++ start_debug.sh
@@ -3,7 +3,4 @@
 meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
-export LD_PRELOAD=$(libtree ./build_debug/lib*.so  \
-      | grep -Eho '\<lib(a|ub|t)san\.so\.[0-9]+\>'  \
-      | sort -u | tr '\n' ' ')
-./poseidon/build_debug/poseidon ./etc
+valgrind -- poseidon ./etc
