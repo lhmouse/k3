8e4d1b3ecff6744ef67eca63b53871834ffaf80c
LIU Hao <lh_mouse@126.com>
2025-05-27 15:15:14 +0800

    common/clock: New

---
+++ k3/common/clock.cpp
@@ -0,0 +1,11 @@
+// This file is part of k3.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved. reserved.
+
+#include "../xprecompiled.hpp"
+#include "clock.hpp"
+#include <time.h>
+namespace k3 {
+
+
+
+}  // namespace k3
---
+++ k3/common/clock.hpp
@@ -0,0 +1,16 @@
+// This file is part of k3.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved. reserved.
+
+#ifndef K3VR5NZE_COMMON_CLOCK_
+#define K3VR5NZE_COMMON_CLOCK_
+
+#include "../fwd.hpp"
+namespace k3 {
+
+class Clock
+  {
+
+  };
+
+}  // namespace k3
+#endif
