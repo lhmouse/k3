e846e2a70d7e700d50db07cee97f995d7d267f9e
LIU Hao <lh_mouse@126.com>
2025-06-06 20:24:36 +0800

    common/message: Add empty class

---
+++ k32/common/message.cpp
@@ -0,0 +1,18 @@
+// This file is part of k32.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved. reserved.
+
+#include "../xprecompiled.hpp"
+#include "message.hpp"
+namespace k32 {
+
+Message::
+Message()
+  {
+  }
+
+Message::
+~Message()
+  {
+  }
+
+}  // namespace k32
