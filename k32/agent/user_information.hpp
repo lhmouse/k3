0ea92a3473c14e5a5ed9dd9e110b3b82dfd11342
LIU Hao <lh_mouse@126.com>
2025-06-16 17:01:42 +0800

    agent/user_information: New

---
+++ k32/agent/user_information.hpp
@@ -0,0 +1,27 @@
+// This file is part of k32.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved. reserved.
+
+#ifndef K32_COMMON_USER_INFORMATION_
+#define K32_COMMON_USER_INFORMATION_
+
+#include "../fwd.hpp"
+#include <poseidon/socket/ipv6_address.hpp>
+namespace k32::agent {
+
+struct User_Information
+  {
+    phcow_string username;
+    ::poseidon::IPv6_Address remote_address;
+
+#ifdef K32_FRIENDS_FC7DDE3B_4A8E_11F0_BB68_5254005015D2_
+    User_Information() noexcept = default;
+#endif
+    User_Information(const User_Information&) = default;
+    User_Information(User_Information&&) = default;
+    User_Information& operator=(const User_Information&) & = default;
+    User_Information& operator=(User_Information&&) & = default;
+    ~User_Information();
+  };
+
+}  // namespace k32::agent
+#endif
