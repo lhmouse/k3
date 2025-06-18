956c280c1f08c9223aad6f33982213a64e524f2f
LIU Hao <lh_mouse@126.com>
2025-06-18 22:25:52 +0800

    common/target_service_descriptor: Remove

--- k32/common/service_future.cpp
+++ k32/common/service_future.cpp
@@ -6,12 +6,16 @@
 #include "service_future.hpp"
 namespace k32 {
 
+constexpr ::poseidon::UUID multicast_uuid = POSEIDON_UUID(c8585663,4c4f,11f0,b169,5254005015d2);
+constexpr ::poseidon::UUID randomcast_uuid = POSEIDON_UUID(c8585775,4c4f,11f0,9b95,5254005015d2);
+constexpr ::poseidon::UUID broadcast_uuid = POSEIDON_UUID(c85857a5,4c4f,11f0,b5da,5254005015d2);
+
 Service_Future::
-Service_Future(const Target_Service_Descriptor& target_descriptor,
-               const cow_string& opcode, const ::taxon::Value& request_data)
+Service_Future(multicast_selector_t&& selector, const cow_string& opcode,
+               const ::taxon::Value& request_data)
   {
-    this->m_target_service_uuid = target_descriptor.service_uuid;
-    this->m_target_service_type = target_descriptor.service_type;
+    this->m_target_service_uuid = selector.target_service_uuid;
+    this->m_target_service_type = move(selector.target_service_type);
     this->m_opcode = opcode;
     this->m_request_data = request_data;
   }
--- k32/common/service_future.hpp
+++ k32/common/service_future.hpp
@@ -5,7 +5,6 @@
 #define K32_COMMON_SERVICE_FUTURE_
 
 #include "../fwd.hpp"
-#include "target_service_descriptor.hpp"
 #include "service_response.hpp"
 #include <poseidon/fiber/abstract_future.hpp>
 #include <taxon.hpp>
@@ -15,6 +14,13 @@ class Service_Future
   :
     public ::poseidon::Abstract_Future
   {
+  public:
+    struct multicast_selector_t
+      {
+        ::poseidon::UUID target_service_uuid;
+        cow_string target_service_type;
+      };
+
   private:
     ::poseidon::UUID m_target_service_uuid;
     cow_string m_target_service_type;
@@ -23,8 +29,8 @@ class Service_Future
     cow_vector<Service_Response> m_responses;
 
   public:
-    Service_Future(const Target_Service_Descriptor& target_descriptor,
-                   const cow_string& opcode, const ::taxon::Value& request_data);
+    Service_Future(multicast_selector_t&& selector, const cow_string& opcode,
+                   const ::taxon::Value& request_data);
 
     Service_Future(const ::poseidon::UUID& target_service_uuid,
                    const cow_string& opcode, const ::taxon::Value& request_data);
@@ -75,6 +81,44 @@ class Service_Future
         return this->m_responses;
       }
   };
+
+extern const ::poseidon::UUID multicast_uuid;
+extern const ::poseidon::UUID randomcast_uuid;
+extern const ::poseidon::UUID broadcast_uuid;
+
+inline
+Service_Future::multicast_selector_t
+multicast(const cow_string& target_service_type)
+  {
+    Service_Future::multicast_selector_t selector;
+    selector.target_service_uuid = multicast_uuid;
+    selector.target_service_type = target_service_type;
+    return selector;
+  }
+
+inline
+Service_Future::multicast_selector_t
+randomcast(const cow_string& target_service_type)
+  {
+    Service_Future::multicast_selector_t selector;
+    selector.target_service_uuid = randomcast_uuid;
+    selector.target_service_type = target_service_type;
+    return selector;
+  }
+
+inline
+const ::poseidon::UUID&
+unicast(const ::poseidon::UUID& target_service_uuid) noexcept
+  {
+    return target_service_uuid;
+  }
+
+inline
+const ::poseidon::UUID&
+broadcast() noexcept
+  {
+    return broadcast_uuid;
+  }
 
 }  // namespace k32
 #endif
