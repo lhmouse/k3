26762f439d4b309f0f305c0a1946ff80edaf6353
LIU Hao <lh_mouse@126.com>
2025-06-19 12:14:46 +0800

    misc: Make GDB run the program upon startup

--- start_debug.sh
+++ start_debug.sh
@@ -4,4 +4,4 @@ meson setup -Ddebug=true -Doptimization=0 -Denable-debug-checks=true build_debug
 meson compile -Cbuild_debug
 
 export LD_LIBRARY_PATH=$(realpath -e build_debug)
-gdb --args poseidon $* ./etc
+gdb -ex run --args poseidon $* ./etc
