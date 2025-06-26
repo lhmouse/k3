d366a395d2d9741e27ef2dffd85cf4769d0c6b68
LIU Hao <lh_mouse@126.com>
2025-06-26 14:37:40 +0800

    role_information: Update

--- k32/monitor/data/role_information.hpp
+++ k32/monitor/data/role_information.hpp
@@ -11,12 +11,11 @@ struct Role_Information
   {
     int64_t roid = 0;
     phcow_string username;
-    phcow_string nickname;
+    cow_string nickname;
     system_time update_time;
     cow_string avatar;
     cow_string profile;
     cow_string whole;
-    ::poseidon::UUID monitor_service_uuid;
 
 #ifdef K32_FRIENDS_3543B0B1_DC5A_4F34_B9BB_CAE513821771_
     Role_Information() noexcept = default;
