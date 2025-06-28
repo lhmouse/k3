1708879f1119f734f08b1894d58c1a0ddabd1210
LIU Hao <lh_mouse@126.com>
2025-06-28 17:24:57 +0800

    clock: Cleanup

--- k32/common/static/clock.hpp
+++ k32/common/static/clock.hpp
@@ -13,17 +13,17 @@ class Clock
   public:
     struct system_time_fields
       {
-        uint32_t year         : 12;  //    0 - 4095
-        uint32_t month        :  4;  //    1 - 12
-        uint32_t day_of_month :  5;  //    1 - 31
-        uint32_t hour         :  5;  //    0 - 23
-        uint32_t minute       :  6;  //    0 - 59
-        uint32_t second       :  6;  //    0 - 60 (leap)
-        uint32_t milliseconds : 10;  //    0 - 999
-        int32_t tz_offset     : 10;  // -720 - 720
-        uint32_t dst          :  1;  // daylight saving time
-        uint32_t day_of_week  :  3;  //    1 - 7
-        uint32_t reserved     :  2;
+        uint32_t year          : 12;  //    0 - 4095
+        uint32_t month         :  4;  //    1 - 12
+        uint32_t day_of_month  :  5;  //    1 - 31
+        uint32_t hour          :  5;  //    0 - 23
+        uint32_t minute        :  6;  //    0 - 59
+        uint32_t second        :  6;  //    0 - 60 (leap)
+        uint32_t milliseconds  : 10;  //    0 - 999
+        int32_t tz_offset      : 10;  // -720 - 720
+        uint32_t dst           :  1;  // daylight saving time
+        uint32_t day_of_week   :  3;  //    1 - 7
+        uint32_t reserved      :  2;
       };
 
   private:
