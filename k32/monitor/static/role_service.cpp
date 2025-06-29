173377f8c39cb80c3a3de50fd3e4d99ea55a36ad
LIU Hao <lh_mouse@126.com>
2025-06-29 18:13:22 +0800

    role_service: Decrease `redis_role_ttl` to 900

--- k32/monitor/static/role_service.cpp
+++ k32/monitor/static/role_service.cpp
@@ -607,7 +607,7 @@ reload(const ::poseidon::Config_File& conf_file)
       this->m_impl = new_sh<X_Implementation>();
 
     // Define default values here. The operation shall be atomic.
-    ::asteria::V_integer redis_role_ttl = 3600;
+    ::asteria::V_integer redis_role_ttl = 900;
 
     // `monitor.redis_role_ttl`
     auto conf_value = conf_file.query(&"monitor.redis_role_ttl");
