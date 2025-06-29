79c34c6da0909f9cff6a77493f071e336e4611ca
LIU Hao <lh_mouse@126.com>
2025-06-29 19:22:03 +0800

    logic/role: Update

---
+++ k32/logic/hydrated/role.cpp
@@ -0,0 +1,43 @@
+// This file is part of k32.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved.
+
+#include "../../xprecompiled.hpp"
+#define K32_FRIENDS_E12697BF_5864_474B_81C5_C0E69BE77D05_
+#include "role.hpp"
+namespace k32::logic {
+
+Role::
+~Role()
+  {
+  }
+
+void
+Role::
+make_avatar(::taxon::V_object& avatar)
+  {
+    avatar.try_emplace(&"avatar_field", 42);
+  }
+
+void
+Role::
+make_profile(::taxon::V_object& profile)
+  {
+    profile.try_emplace(&"profile", &"meow");
+  }
+
+void
+Role::
+hydrate(const ::taxon::V_object& db_data)
+  {
+    POSEIDON_LOG_FATAL(("HYDRATE: $1: $2"), *this, db_data);
+  }
+
+void
+Role::
+hibernate(::taxon::V_object& db_data)
+  {
+    db_data.try_emplace(&"db_data", true);
+    POSEIDON_LOG_FATAL(("HIBERNATE: $1: $2"), *this, db_data);
+  }
+
+}  // namespace k32::logic
---
+++ k32/logic/hydrated/role.hpp
@@ -0,0 +1,70 @@
+// This file is part of k32.
+// Copyright (C) 2024-2025, LH_Mouse. All wrongs reserved.
+
+#ifndef K32_LOGIC_HYDRATED_ROLE_
+#define K32_LOGIC_HYDRATED_ROLE_
+
+#include "../../fwd.hpp"
+namespace k32::logic {
+
+class Role
+  {
+  private:
+    // basic information
+    ::poseidon::UUID m_agent_service_uuid;
+    int64_t m_roid = 0;
+    cow_string m_nickname;
+    phcow_string m_username;
+
+  public:
+#ifdef K32_FRIENDS_E12697BF_5864_474B_81C5_C0E69BE77D05_
+    friend class Role_Service;
+    Role() noexcept = default;
+#endif
+    Role(const Role&) = delete;
+    Role& operator=(const Role&) & = delete;
+    ~Role();
+
+    // Get basic information. These fields are read-only.
+    int64_t
+    roid() const noexcept
+      { return this->m_roid;  }
+
+    const cow_string&
+    username() const noexcept
+      { return this->m_username;  }
+
+    const cow_string&
+    nickname() const noexcept
+      { return this->m_nickname;  }
+
+    // Create an avatar of this role. This is what others can see outdoors.
+    void
+    make_avatar(::taxon::V_object& avatar);
+
+    // Create a profile snapshot of this role. This is what others can see on
+    // the profile page.
+    void
+    make_profile(::taxon::V_object& profile);
+
+#ifdef K32_FRIENDS_E12697BF_5864_474B_81C5_C0E69BE77D05_
+    // Load role cultivation data from the database. This is an internal function
+    // called by the role service, after basic information has been filled in.
+    void
+    hydrate(const ::taxon::V_object& db_data);
+#endif
+
+    // Serialize role cultivation data which can then be stored into the database.
+    void
+    hibernate(::taxon::V_object& db_data);
+  };
+
+inline
+tinyfmt&
+operator<<(tinyfmt& fmt, const Role& role)
+  {
+    return format(fmt, "role `$1` (`$2`)", role.roid(), role.nickname());
+  }
+
+}  // namespace k32::logic
+#endif
