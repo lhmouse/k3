38da9956e38980995a7041e6fdf637a98b5afaf0
LIU Hao <lh_mouse@126.com>
2025-06-30 11:24:14 +0800

    logic/role_service: Add config

--- k32/logic/static/role_service.cpp
+++ k32/logic/static/role_service.cpp
@@ -6,13 +6,15 @@
 #include "role_service.hpp"
 #include "../globals.hpp"
 #include <poseidon/base/config_file.hpp>
+#include <asteria/library/chrono.hpp>
 namespace k32::logic {
 namespace {
 
 struct Implementation
   {
+    int service_zone_id = 0;
+    system_time service_start_time;
   };
-
 
 }  // namespace
 
@@ -29,6 +31,26 @@ Role_Service::
   {
   }
 
+int
+Role_Service::
+service_zone_id() const noexcept
+  {
+    if(!this->m_impl)
+      return 0;
+
+    return this->m_impl->service_zone_id;
+  }
+
+system_time
+Role_Service::
+service_start_time() const noexcept
+  {
+    if(!this->m_impl)
+      return system_time();
+
+    return this->m_impl->service_start_time;
+  }
+
 void
 Role_Service::
 reload(const ::poseidon::Config_File& conf_file)
@@ -36,6 +58,55 @@ reload(const ::poseidon::Config_File& conf_file)
     if(!this->m_impl)
       this->m_impl = new_sh<X_Implementation>();
 
+    // Define default values here. The operation shall be atomic.
+    int64_t service_zone_id = 0, service_start_time_ms = 0;
+
+    // `logic.service_zone_id`
+    auto conf_value = conf_file.query(&"logic.service_zone_id");
+    if(conf_value.is_integer())
+      service_zone_id = conf_value.as_integer();
+    else if(!conf_value.is_null())
+      POSEIDON_THROW((
+          "Invalid `logic.service_zone_id`: expecting an `integer`, got `$1`",
+          "[in configuration file '$2']"),
+          conf_value, conf_file.path());
+
+    if((service_zone_id < 1) || (service_zone_id > 99999999))
+      POSEIDON_THROW((
+          "Invalid `logic.service_zone_id`: value `$1` out of range",
+          "[in configuration file '$2']"),
+          service_zone_id, conf_file.path());
+
+    // `logic.service_start_time`
+    conf_value = conf_file.query(&"logic.service_start_time");
+    if(conf_value.is_string())
+      service_start_time_ms = ::asteria::std_chrono_parse(conf_value.as_string());
+    else if(!conf_value.is_null())
+      POSEIDON_THROW((
+          "Invalid `logic.service_start_time`: expecting a `string`, got `$1`",
+          "[in configuration file '$2']"),
+          conf_value, conf_file.path());
+
+    if((service_start_time_ms < 915148800'000) || (service_start_time_ms > 4102444800'000))
+      POSEIDON_THROW((
+          "Invalid `logic.service_start_time`: value `$1` out of range",
+          "[in configuration file '$2']"),
+          service_start_time_ms, conf_file.path());
+
+    // Set up new configuration. This operation shall be atomic.
+    this->m_impl->service_zone_id = static_cast<int>(service_zone_id);
+    this->m_impl->service_start_time = system_time(milliseconds(service_start_time_ms));
+
+    // Set up request handlers.
+//    service.set_handler(&"/user/kick", bindw(this->m_impl, do_slash_user_kick));
+//    service.set_handler(&"/user/ban/set", bindw(this->m_impl, do_slash_user_ban_set));
+//    service.set_handler(&"/user/ban/lift", bindw(this->m_impl, do_slash_user_ban_lift));
+//    service.set_handler(&"/nickname/acquire", bindw(this->m_impl, do_slash_nickname_acquire));
+//    service.set_handler(&"/nickname/release", bindw(this->m_impl, do_slash_nickname_release));
+
+    // Restart the service.
+//    this->m_impl->service_timer.start(150ms, 7001ms, bindw(this->m_impl, do_service_timer_callback));
+//    this->m_impl->user_server.start(this->m_impl->client_port, bindw(this->m_impl, do_server_hws_callback));
 
   }
 
--- k32/logic/static/role_service.hpp
+++ k32/logic/static/role_service.hpp
@@ -21,6 +21,17 @@ class Role_Service
     Role_Service& operator=(const Role_Service&) & = delete;
     ~Role_Service();
 
+    // Gets the pre-configured zone ID of this service. This can be used to
+    // identify individual services within the same cluster. If no service is
+    // running, zero is returned.
+    int
+    service_zone_id() const noexcept;
+
+    // Gets the pre-configured start time of this service. If no service is
+    // running, '1970-01-01T00:00:00Z' is returned.
+    system_time
+    service_start_time() const noexcept;
+
     // Reloads configuration.
     void
     reload(const ::poseidon::Config_File& conf_file);
